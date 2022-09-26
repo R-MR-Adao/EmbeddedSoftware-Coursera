61dc9c1df890daab72d5a000a5c3c69dacb7796c
R-MR-Adao <rm.ribeiro.adao@gmail.com>
2022-09-26 22:51:40 +0100

    Adding code outline and documentation for C1M1 assignment

--- week1-ApplicationAssignment/stats.c
+++ week1-ApplicationAssignment/stats.c
@@ -9,27 +9,36 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Perform some basic statiscs analysis on a simple dataset
  *
- * <Add Extended Description Here>
+ *  - main() - The main entry point for your program
+ *  - print_statistics() - A function that prints the statistics of an array including minimum, maximum, mean, and median.
+ *  - print_array()   - Given an array of data and a length, prints the array to the screen
+ *  - find_median()   - Given an array of data and a length, returns the median value
+ *  - find_mean()     - Given an array of data and a length, returns the mean
+ *  - find_maximum()  - Given an array of data and a length, returns the maximum
+ *  - find_minimum()  - Given an array of data and a length, returns the minimum
+ *  - sort_array()    - Given an array of data and a length, sorts the array from largest to smallest.  (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Ricardo Adao
+ * @date 2022-09-22
  *
  */
 
 
 
 #include <stdio.h>
+#include <stdint.h>
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
 
+
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+  uint8_t test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
@@ -41,3 +50,17 @@ void main() {
 }
 
 /* Add other Implementation File Code Here */
+
+void print_statistics(uint8_t * arr, uint8_t arr_size);
+
+void print_array(uint8_t * arr, uint8_t arr_size);
+
+uint8_t find_median(uint8_t * arr, uint8_t arr_size);
+
+uint8_t find_mean(uint8_t * arr, uint8_t arr_size);
+
+uint8_t find_maximum(uint8_t * arr, uint8_t arr_size);
+
+uint8_t find_minimum(uint8_t * arr, uint8_t arr_size);
+
+uint8_t sort_array(uint8_t * arr, uint8_t arr_size);
--- week1-ApplicationAssignment/stats.h
+++ week1-ApplicationAssignment/stats.h
@@ -9,13 +9,20 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Perform some basic statiscs analysis on a simple dataset
  *
- * <Add Extended Description Here>
+ *  - main() - The main entry point for your program
+ *  - print_statistics() - A function that prints the statistics of an array including minimum, maximum, mean, and median.
+ *  - print_array()   - Given an array of data and a length, prints the array to the screen
+ *  - find_median()   - Given an array of data and a length, returns the median value
+ *  - find_mean()     - Given an array of data and a length, returns the mean
+ *  - find_maximum()  - Given an array of data and a length, returns the maximum
+ *  - find_minimum()  - Given an array of data and a length, returns the minimum
+ *  - sort_array()    - Given an array of data and a length, sorts the array from largest to smallest.  (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Ricardo Adao
+ * @date 2022-09-22
  *
  */
 #ifndef __STATS_H__
@@ -24,17 +31,84 @@
 /* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Print array statistics
  *
- * <Add Extended Description Here>
+ * A function that prints the statistics of an array including minimum, maximum, mean, and median.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @param[in] arr the input array.
+ * @param[in] arr_size the size of the array.
  */
+void print_statistics(uint8_t * arr, uint8_t arr_size);
+
+/**
+ * @brief Print array data
+ *
+ * Given an array of data and a length, prints the array to the screen.
+ *
+ * @param[in] arr the input array.
+ * @param[in] arr_size the size of the array.
+ */
+void print_array(uint8_t * arr, uint8_t arr_size);
+
+/**
+ * @brief Find array median
+ *
+ * Given an array of data and a length, returns the median value.
+ *
+ * @param[in] arr the input array.
+ * @param[in] arr_size the size of the array.
+ *
+ * @return median the median of the array.
+ */
+uint8_t find_median(uint8_t * arr, uint8_t arr_size);
+
+/**
+ * @brief Find array mean
+ *
+ * Given an array of data and a length, returns the mean value.
+ *
+ * @param[in] arr the input array.
+ * @param[in] arr_size the size of the array.
+ *
+ * @return mean the mean of the array.
+ */
+uint8_t find_mean(uint8_t * arr, uint8_t arr_size);
+
+/**
+ * @brief Find array maximum
+ *
+ * Given an array of data and a length, returns the maximum value.
+ *
+ * @param[in] arr the input array.
+ * @param[in] arr_size the size of the array.
+ *
+ * @return maximum the maximum of the array.
+ */
+uint8_t find_maximum(uint8_t * arr, uint8_t arr_size);
+
+/**
+ * @brief Find array minimum
+ *
+ * Given an array of data and a length, returns the minimum value.
+ *
+ * @param[in] arr the input array.
+ * @param[in] arr_size the size of the array.
+ *
+ * @return minimum the minimum of the array.
+ */
+uint8_t find_minimum(uint8_t * arr, uint8_t arr_size);
+
+/**
+ * @brief Descending sort array
+ *
+ * Given an array of data and a length, sorts the array from largest to smallest.
+ *
+ * @param[in/out] arr the input array.
+ * @param[in] arr_size the size of the array.
+ *
+ * @return sort the median of the array.
+ */
+uint8_t sort_array(uint8_t * arr, uint8_t arr_size);
 
 
 #endif /* __STATS_H__ */
