784649d17cc492acf13deaac8922b31527e33abe
R-MR-Adao <rm.ribeiro.adao@gmail.com>
2022-10-02 00:01:18 +0100

    Specifying compilation routine using GNU MAKE

--- week2-ApplicationAssignment/src/platform.mk
+++ week2-ApplicationAssignment/src/platform.mk
@@ -1,10 +1,10 @@
 #******************************************************************************
-# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+# Copyright (C) 2022 by Ricardo Adao
 #
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
-# software. Alex Fosdick and the University of Colorado are not liable for any
+# software. Ricardo Adao is not liable for any
 # misuse of this material.
 #
 #*****************************************************************************
@@ -14,22 +14,5 @@
 # date: 2022-10-01
 #*****************************************************************************
 
-
-include platform.mk
-
-ifeq ($(PLATFORM),MSP432)
-	SOURCES = main.c \
-		memory.c \
-		interrupts_msp432p401r_gcc.c \
-		startup_msp432p401r_gcc.c \
-		system_msp432p401r.c
-endif
-
-ifeq ($(PLATFORM),HOST)
-	SOURCES = main.c \
-		memory.c
-endif
-
-INCLUDES = -I../include/common \
-	-I../include/CMSIS \
-	-I../include/msp432
+# Platform Overrides
+PLATFORM = HOST
